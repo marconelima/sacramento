03f7bc5e83cba03949bf9c5808b7788529e85cb5
marconelima <marcone.lima@gmail.com>
2021-12-16 08:20:33 -0300

    order codigo api

--- painel/paginas/produto.php
+++ painel/paginas/produto.php
@@ -1922,3 +1922,17 @@ if (isset($_POST['novo_campo'])) {
 <?php
 }
 ?>
+<script>
+    if (document.querySelector("#form-search")) {
+        let form = document.querySelector("#form-search");
+        let campo = document.querySelector("#order");
+        let colunas = document.querySelectorAll(".orderBy");
+
+        colunas.forEach(function(el) {
+            el.addEventListener("click", (e) => {
+                campo.value = el.id;
+                form.submit();
+            });
+        });
+    }
+</script>
