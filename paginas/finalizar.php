cdf88d84ae0479d8a78466053c17d9f80b6ae408
marconelima <marcone.lima@gmail.com>
2021-12-28 18:11:53 -0300

    config ambiente teste

--- paginas/finalizar.php
+++ paginas/finalizar.php
@@ -235,12 +235,12 @@
             //Server settings
             $mail->SMTPDebug = 0;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.uhserver.com';                     //Set the SMTP server to send through
+            $mail->Host       = 'smtps.uhserver.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'noreply@industriasacramento.com.br';                     //SMTP username
             $mail->Password   = 'G4p2f5D3@';                               //SMTP password
             $mail->SMTPSecure = '';            //Enable implicit TLS encryption
-            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
             $mail->setFrom($rs_configuracao['emailloja'], utf8_decode($rs_configuracao['nomeloja']));
@@ -293,12 +293,12 @@
                     //Server settings
                     $mail->SMTPDebug = 0;                      //Enable verbose debug output
                     $mail->isSMTP();                                            //Send using SMTP
-                    $mail->Host       = 'smtp.uhserver.com';                     //Set the SMTP server to send through
+                    $mail->Host       = 'smtps.uhserver.com';                     //Set the SMTP server to send through
                     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                     $mail->Username   = 'noreply@industriasacramento.com.br';                     //SMTP username
                     $mail->Password   = 'G4p2f5D3@';                               //SMTP password
                     $mail->SMTPSecure = '';            //Enable implicit TLS encryption
-                    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                     //Recipients
                     $mail->setFrom($rs_configuracao['emailloja'], utf8_decode($rs_configuracao['nomeloja']));
