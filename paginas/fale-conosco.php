6892407b7eb93acb53d47b7864bd7b0925d8dd69
marconelima <marcone.lima@gmail.com>
2021-12-29 09:48:14 -0300

    config ambiente teste

--- paginas/fale-conosco.php
+++ paginas/fale-conosco.php
@@ -122,7 +122,7 @@
 
             try {
                 //Server settings
-                $mail->SMTPDebug = 0;                      //Enable verbose debug output
+                $mail->SMTPDebug = 2;                      //Enable verbose debug output
                 $mail->isSMTP();                                            //Send using SMTP
                 $mail->Host       = 'smtp.uhserver.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
