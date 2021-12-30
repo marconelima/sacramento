2285ca7ff6131f1e723ada24a03d3a28e6d76c47
marconelima <marcone.lima@gmail.com>
2021-12-30 09:40:57 -0300

    config ambiente teste

--- paginas/fale-conosco.php
+++ paginas/fale-conosco.php
@@ -124,7 +124,7 @@
                 //Server settings
                 $mail->SMTPDebug = 2;                      //Enable verbose debug output
                 $mail->isSMTP();                                            //Send using SMTP
-                $mail->Host       = 'smtp.'.substr(strstr($caixaPostalServidorEmail, '@'), 1);;                     //Set the SMTP server to send through
+                $mail->Host       = 'smtps.uhserver.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'noreply@industriasacramento.com.br';                     //SMTP username
                 $mail->Password   = 'G4p2f5D3@';                               //SMTP password
