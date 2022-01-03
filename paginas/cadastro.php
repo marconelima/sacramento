0ab3bf87dbcef9d7dc27814c668eb8ac35ac3848
marconelima <marcone.lima@gmail.com>
2022-01-03 10:09:10 -0300

    cpf ou cpnj

--- paginas/cadastro.php
+++ paginas/cadastro.php
@@ -63,7 +63,7 @@ if (isset($_SESSION['cliente']) && @$_SESSION['cliente'] != '') {
                                 <label>CPF / CNPJ</label>
                                 <input class="form-control" required="required" name="cnpj" id="nucnpj2" value="<?php echo @$rs_cliente['cnpj']; ?>" placeholder="">
                             </div>
-                            <div class="form-group col-12 col-md-4" id="inscricaoest" <?php if(@$rs_cliente['tipodocumento'] == 'cnpj'){ ?>style="display: block;" <?php }  else { ?> style="display: none;" <?php } ?> >
+                            <div class="form-group col-12 col-md-4" id="inscricaoest2" <?php if(@$rs_cliente['tipodocumento'] == 'cnpj'){ ?>style="display: block;" <?php }  else { ?> style="display: none;" <?php } ?> >
                                 <label>Inscrição Estadual</label>
                                 <input class="form-control" name="inscricaoestadual" id="inscricaoestadual2" value="<?php echo @$rs_cliente['inscricaoestadual']; ?>" placeholder="">
                             </div>
