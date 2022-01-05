ab2c4ae965b9ae4a2ad78e38239b25a3dac43655
marconelima <marcone.lima@gmail.com>
2022-01-05 15:34:00 -0300

    orçamento painel

--- painel/paginas/relatorio_pedido.php
+++ painel/paginas/relatorio_pedido.php
@@ -1,5 +1,5 @@
-<?php error_reporting(0);
-ini_set("display_errors", 0);
+<?php error_reporting(1);
+ini_set("display_errors", 1);
 
 
 if (isset($_POST['relatorio_geral']) && $_POST['relatorio_geral'] == "Gerar PDF") {
