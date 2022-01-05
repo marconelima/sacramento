269816063058d9b12413e23132f275a16353fc7f
marconelima <marcone.lima@gmail.com>
2022-01-05 17:18:36 -0300

    orçamento painel

--- header.php
+++ header.php
@@ -49,7 +49,7 @@ if (isset($_POST['sair']) && @$_POST['sair'] == "1") {
 if (isset($sair) && $sair == 1) {
     unset($_SESSION['cliente'], $_SESSION['nome_cliente'], $_SESSION['email_cliente'], $_SESSION['cpf_cliente'], $_SESSION['telefone_cliente']);
 
-    header("Location: /");
+    header("Location: ".$siteUrl."/catalogo/21");
 }
 
 if (isset($_POST['entrar']) && $_POST['entrar'] == "Login") {
